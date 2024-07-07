59366a3b945dd62665dd81aa698be4e8804975f3
YahyaShapan <105345162+YahyaShapan@users.noreply.github.com>
2024-07-08 02:53:54 +0300

    Update settings.py

--- da/settings.py
+++ da/settings.py
@@ -99,23 +99,23 @@ WSGI_APPLICATION = 'da.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'IQurQaxLAHNOEctjgKOzkkIqHtKIWzbt',
-        'HOST': 'viaduct.proxy.rlwy.net',
-        'PORT': '52123',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'IQurQaxLAHNOEctjgKOzkkIqHtKIWzbt',
+#         'HOST': 'viaduct.proxy.rlwy.net',
+#         'PORT': '52123',
+#     }
+# }
 
 
 # Password validation
